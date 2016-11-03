653a0a90cceb4410eb6f7f430dbf734b20c93a89
ChristopherWilkesMPM <cw646@cam.ac.uk>
2016-11-03 22:11:57 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,45 @@
-//Copyright (c) [2016] [CB-Geo FE-Lib]
+// Copyright (c) [2016] [CB-Geo FE-Lib]
 
+#include <fstream>
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <vector>
 
 int main() {
-    
+
   // Following Values need to be cin'd at some point, values are test case.
-    
-    const double xlength=5.;
-    const double yheight=5.;
-    const double xspacing = 1;
-    const double yspacing = 1;
-    
-   // NI = Number of nodes in I Direction
-   // NJ = Number of nodes in J Direction
 
-    int NI = ((double)xlength/xspacing)+1;
-    int NJ = ((double)yheight/yspacing)+1;
-    
+  const double xlength = 5.;
+  const double yheight = 5.;
+  const double xspacing = 1;
+  const double yspacing = 1;
 
-    std::vector<std::pair<int,int>> Coord; //Coordinate output
-    
-    for (int i = 0; i < NI; ++i)
-    {
-        for (int j = 0; j < NJ; ++j)
-        {
-            Coord.push_back(std::make_pair(i,j));
-        }
+  int NI =
+      ((double)xlength / xspacing) + 1; // NI = Number of nodes in I Direction
+  int NJ =
+      ((double)yheight / yspacing) + 1; // NJ = Number of nodes in J Direction
+
+  std::vector<std::pair<int, int>> Coord;   // Coordinate output
+  std::fstream outfile;                     // File stream name
+  std::string outputfilename = "Coord.txt"; // File name and type
+
+  outfile.open(outputfilename, std::ios::out);
+
+  if (outfile.is_open()) {
+    for (int i = 0; i < NI; ++i) {
+      for (int j = 0; j < NJ; ++j) {
+        Coord.push_back(std::make_pair(i, j));
+      }
     }
-    
-    for (const auto &coord : Coord) std::cout << coord.first <<","<< coord.second << '\n';
-    
+
+    for (const auto &coord : Coord)
+      outfile << coord.first << "," << coord.second << '\n';
+    outfile.close();
+  }
+
+  else {
+    std::cout << "Could not create file:" << outputfilename << std::endl;
+  }
 }
 
 // Status : Can print coordinates of points to console
-
