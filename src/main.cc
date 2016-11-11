a6fab43e606a8c7eba5ed6fc5cbdf008fd9ae9b2
Krishna Kumar <kks32@cam.ac.uk>
2016-11-11 11:36:40 +0000

    Using array to store coordinates

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <array>
 #include <fstream>
 #include <iostream>
 #include <utility>
@@ -13,26 +14,26 @@ int main() {
   const double yspacing = 1.;
 
   // NI = Number of nodes in I Direction
-  int NI = ((double)xlength / xspacing) + 1;
+  const int ni = (static_cast<double>(xlength / xspacing)) + 1;
   // NJ = Number of nodes in J Direction
-  int NJ =
-      ((double)yheight / yspacing) + 1;
-
-  std::vector<std::pair<int, int>> coords;    // Coordinate output
+  const int nj = (static_cast<double>(yheight / yspacing)) + 1;
+  // Coordinate output
+  std::vector<std::array<double, 2>> coords;
   std::fstream outfile;                      // Output file stream name
   std::string outputfilename = "coord.txt";  // Output file name and type
+
+  for (unsigned i = 0; i < ni; ++i) {
+    for (unsigned j = 0; j < nj; ++j) {
+      std::array<double, 2> coord = {i * xspacing, j * yspacing};
+      coords.push_back(coord);
+    }
+  }
 
   outfile.open(outputfilename, std::ios::out);
 
   if (outfile.is_open()) {
-    for (int i = 0; i < NI; ++i) {
-      for (int j = 0; j < NJ; ++j) {
-        coords.push_back(std::make_pair(i, j));
-      }
-    }
-
     for (const auto& coord : coords)
-      outfile << coord.first << "," << coord.second << '\n';
+      outfile << coord.at(0) << "," << coord.at(1) << '\n';
     outfile.close();
   }
 
