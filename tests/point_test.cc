80946a1665fe7fb9871d5e065190093fa851643c
Ezra Tjung <ezrayst@berkeley.edu>
2017-05-25 18:46:40 -0700

    :computer: add test for point class

--- tests/point_test.cc
+++ tests/point_test.cc
@@ -1,15 +1,22 @@
-// Pointbase test
+// Point test
+#include <array>
 #include <limits>
 
 #include "catch.hpp"
 #include "point.h"
 
-//! \brief Check node base class for 1D case
-TEST_CASE("Point base is checked for 1D case", "[point][2D]") {
-  //! Check for id = 0
-  SECTION("Point id is zero") {
-    // long long id = 0;
-    // auto node = std::make_shared<felib::Point>(id, coords);
-    // REQUIRE(node->id() == 0);
-  }
+//! \brief Check that Point will store right values
+//! \details Write arbritrary index and coordinate and store them
+
+TEST_CASE("Point base is checked in 3D", "[Point][3D]") {
+
+  const unsigned Tdim = 3;
+  unsigned index = 0;
+  const std::array<double, Tdim> coordinate{0, 0, 0};
+  auto node = std::unique_ptr<Point<Tdim>>(new Point<Tdim>(index, coordinate));
+
+  REQUIRE(node->id() == 0);
+  REQUIRE(node->coordinates().at(0) == 0);
+  REQUIRE(node->coordinates().at(1) == 0);
+  REQUIRE(node->coordinates().at(2) == 0);
 }
