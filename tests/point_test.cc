6d92a39d25314686d3a80a9c175f788dd3c2c38a
Ezra Tjung <ezrayst@berkeley.edu>
2017-05-31 20:44:57 -0700

    :dart: add 2d and 3d test, fix issue #4

--- tests/point_test.cc
+++ tests/point_test.cc
@@ -8,15 +8,28 @@
 //! \brief Check that Point will store right values
 //! \details Write arbritrary index and coordinate and store them
 
-TEST_CASE("Point base is checked in 3D", "[Point][3D]") {
+TEST_CASE("Point base is checked in 2D and 3D", "[Point][2D][3D]") {
 
-  const unsigned Tdim = 3;
-  unsigned index = 0;
-  const std::array<double, Tdim> coordinate{0, 0, 0};
-  auto node = std::unique_ptr<Point<Tdim>>(new Point<Tdim>(index, coordinate));
+  //! test for 3d
+  const unsigned Tdim_3d = 3;
+  unsigned index_3d = 0;
+  const std::array<double, Tdim_3d> coordinate_3d{0, 0, 0};
+  std::unique_ptr<Point<Tdim_3d>> node_3d(
+      new Point<Tdim_3d>(index_3d, coordinate_3d));
 
-  REQUIRE(node->id() == 0);
-  REQUIRE(node->coordinates().at(0) == 0);
-  REQUIRE(node->coordinates().at(1) == 0);
-  REQUIRE(node->coordinates().at(2) == 0);
+  REQUIRE(node_3d->id() == 0);
+  REQUIRE(node_3d->coordinates().at(0) == 0);
+  REQUIRE(node_3d->coordinates().at(1) == 0);
+  REQUIRE(node_3d->coordinates().at(2) == 0);
+
+  //! test for 2d
+  const unsigned Tdim_2d = 2;
+  unsigned index_2d = 1;
+  const std::array<double, Tdim_2d> coordinate_2d{1, 1};
+  std::unique_ptr<Point<Tdim_2d>> node_2d(
+      new Point<Tdim_2d>(index_2d, coordinate_2d));
+
+  REQUIRE(node_2d->id() == 1);
+  REQUIRE(node_2d->coordinates().at(0) == 1);
+  REQUIRE(node_2d->coordinates().at(1) == 1);
 }
