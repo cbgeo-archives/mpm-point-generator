975481de50afaa8da63786f4d50f37d9eb50c27d
ezrayst <ezrayst@berkeley.edu>
2017-06-07 12:38:45 -0700

    :dart: Test point class (#16)
    * :computer: add test for point class
    
    * :dart: add 2d and 3d test, fix issue #4
    
    * :dart: add more tests for extreme values and zeros
    
    * :dart: fix issue #1, for point class with sections
    
    * :dart: fix issue #4, PR #16 for point class
    
    * :dart: make 2 test cases, for issue #4 and PR#16
    
    * :dart: fix PR #16

--- tests/point_test.cc
+++ tests/point_test.cc
@@ -1,15 +1,107 @@
-// Pointbase test
+// Point test
+#include <array>
 #include <limits>
 
 #include "catch.hpp"
 #include "point.h"
 
-//! \brief Check node base class for 1D case
-TEST_CASE("Point base is checked for 1D case", "[point][2D]") {
-  //! Check for id = 0
-  SECTION("Point id is zero") {
-    // long long id = 0;
-    // auto node = std::make_shared<felib::Point>(id, coords);
-    // REQUIRE(node->id() == 0);
+//! \brief Check point class in 2D
+TEST_CASE("Point base is checked in 2D", "[Point][2D]") {
+
+  //! 2D Test of Point Class
+  const double tolerance = 1.E-12;
+  const unsigned DIM = 2;
+
+  //! Coordinates are checked for zero values
+  SECTION("Coordinates are checked for zero values") {
+    const unsigned index = 0;
+    const std::array<double, DIM> coordinate{0, 0};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == 0);
+    REQUIRE(node->coordinates().at(0) == Approx(0).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) == Approx(0).epsilon(tolerance));
+  }
+
+  //! Coordinates are checked for minimum values
+  SECTION("Coordinates are checked for minimum values") {
+    const unsigned index = std::numeric_limits<unsigned>::min();
+    const std::array<double, DIM> coordinate{
+        std::numeric_limits<double>::min(), std::numeric_limits<double>::min()};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == std::numeric_limits<unsigned>::min());
+    REQUIRE(node->coordinates().at(0) ==
+            Approx(std::numeric_limits<double>::min()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) ==
+            Approx(std::numeric_limits<double>::min()).epsilon(tolerance));
+  }
+
+  //! Coordinates are checked for maximum values
+  SECTION("Coordinates are checked for maximum values") {
+    const unsigned index = std::numeric_limits<unsigned>::max();
+    const std::array<double, DIM> coordinate{
+        std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == std::numeric_limits<unsigned>::max());
+    REQUIRE(node->coordinates().at(0) ==
+            Approx(std::numeric_limits<double>::max()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) ==
+            Approx(std::numeric_limits<double>::max()).epsilon(tolerance));
+  }
+}
+
+//! \brief Check point class in 3D
+TEST_CASE("Point base is checked in 3D", "[Point][3D]") {
+
+  //! 3D Test of Point Class
+  const double tolerance = 1.E-12;
+  const unsigned DIM = 3;
+
+  //! Coordinates are checked for zero values
+  SECTION("Coordinates are checked for zero values") {
+    const unsigned index = 0;
+    const std::array<double, DIM> coordinate{0, 0, 0};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == 0);
+    REQUIRE(node->coordinates().at(0) == Approx(0).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) == Approx(0).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(2) == Approx(0).epsilon(tolerance));
+  }
+
+  //! Coordinates are checked for minimum values
+  SECTION("Coordinates are checked for minimum values") {
+    const unsigned index = std::numeric_limits<unsigned>::min();
+    const std::array<double, DIM> coordinate{
+        std::numeric_limits<double>::min(), std::numeric_limits<double>::min(),
+        std::numeric_limits<double>::min()};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == std::numeric_limits<unsigned>::min());
+    REQUIRE(node->coordinates().at(0) ==
+            Approx(std::numeric_limits<double>::min()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) ==
+            Approx(std::numeric_limits<double>::min()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(2) ==
+            Approx(std::numeric_limits<double>::min()).epsilon(tolerance));
+  }
+
+  //! Coordinates are checked for maximum values
+  SECTION("Coordinates are checked for maximum values") {
+    const unsigned index = std::numeric_limits<unsigned>::max();
+    const std::array<double, DIM> coordinate{
+        std::numeric_limits<double>::max(), std::numeric_limits<double>::max(),
+        std::numeric_limits<double>::max()};
+    auto node = std::unique_ptr<Point<DIM>>(new Point<DIM>(index, coordinate));
+
+    REQUIRE(node->id() == std::numeric_limits<unsigned>::max());
+    REQUIRE(node->coordinates().at(0) ==
+            Approx(std::numeric_limits<double>::max()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(1) ==
+            Approx(std::numeric_limits<double>::max()).epsilon(tolerance));
+    REQUIRE(node->coordinates().at(2) ==
+            Approx(std::numeric_limits<double>::max()).epsilon(tolerance));
   }
 }
