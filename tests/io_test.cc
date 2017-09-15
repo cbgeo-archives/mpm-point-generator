c911afb6c546e102d54e05e5f21138ed42bb86a1
Ezra Tjung <ezrayst@berkeley.edu>
2017-09-15 00:10:56 -0700

    :wrench: change io test

--- tests/io_test.cc
+++ tests/io_test.cc
@@ -10,7 +10,4 @@
 TEST_CASE("IO is checked in 3D", "[IO][3D]") {
 
   //! 3D Test of IO Class
-  const unsigned DIM = 3;
-  auto io = std::unique_ptr<IO<DIM>> (new IO<DIM>("../bin/test_3d.msh"));
-  REQUIRE(io->mesh_file_name() == "../bin/test_3d.msh");
 }
