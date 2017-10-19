66080b08261b0f1eca5b1b8e463d0ade7e4a5bc1
cw646 <cw646@cam.ac.uk>
2017-10-19 15:06:27 +0100

    :computer: Added support for initial volume calculation

--- include/mesh.h
+++ include/mesh.h
@@ -47,6 +47,12 @@ class Mesh {
   //! Return the total number of vertices
   unsigned nvertices() const { return nvertices_; }
 
+  //! Return a map of element id & volume id
+  std::map<unsigned, double> volumes() const { return volumes_; }
+
+  //! Compute the volume of a material point
+  void compute_volumes();
+
  protected:
   //! Total number of vertices
   unsigned nvertices_{std::numeric_limits<unsigned>::max()};
@@ -65,6 +71,12 @@ class Mesh {
 
   //! Vector of material points
   std::vector<std::unique_ptr<MaterialPoints<Tdim>>> materialpoints_;
+
+  //! Map to store element id and volume
+  std::map<unsigned, double> volumes_;
+
+  //! Return volume
+  double calculate_volume(const Eigen::MatrixXd& m);
 };
 
 #include "mesh.tcc"
